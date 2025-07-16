ad20788c78ca8f86db489546f0cd0cb595dd9988
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-07-16 10:30:54 +0000

    [Fix] application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module MeshiterroSlim
   class Application < Rails::Application
-    config.generators.templete_engine = slim
+    config.generators.template_engine = :slim
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
