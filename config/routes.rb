11fde1e590b7ca09f2d2ee05ed8ae2a48b03f779
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-08-04 12:28:59 +0000

    [Add] devise sign_up sign_in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
-  get 'homes/about'
+  get 'homes/about'=>'homes#about', as:'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_07_30_140634) do
+ActiveRecord::Schema.define(version: 2025_07_30_142254) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
