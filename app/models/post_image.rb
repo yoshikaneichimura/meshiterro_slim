1a9b994696cd4e3d3b0677644377e8d78df056ca
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-09-15 13:28:25 +0000

    [Add] favorite_action

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,13 +1,18 @@
 class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
-  has_many :post_comments, dependent: :destroy
-  
+  has_many  :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/image/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image
-  end  
+  end
 end
